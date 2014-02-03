f6848aa33dcbd1bee5187de5a455c20bc90192b7
Andrew Wei <andey.wei@gmail.com>
2014-02-02 22:53:03 -0600

    more admin panel edits

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,8 +1,28 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id              :integer          not null, primary key
+#  reddit_id       :string(255)      not null
+#  title           :string(255)      not null
+#  sub_id          :integer          not null
+#  tumblr_id       :string(255)
+#  content_type_id :integer
+#  submitted_at    :datetime
+#  created_at      :datetime
+#  updated_at      :datetime
+#  json            :hstore
+#  whitelist_id    :integer
+#  garbage         :boolean
+#  processed_at    :datetime
+#
+
 ActiveAdmin.register Post do
   config.sort_order = "created_at_desc"
   actions :all, :except => [:destroy]
 
   filter :title
+  filter :reddit_id
   filter :sub
   filter :content_type_id
   filter :garbage
@@ -15,5 +35,20 @@ ActiveAdmin.register Post do
     column :submitted_at
     column :garbage
     default_actions
+  end
+
+  show do |ama|
+    attributes_table do
+      row :title
+      row :sub
+      row :reddit_id
+      row :tumblr_id
+      row :created_at
+      row :processed_at
+      row :submitted_at
+      row :garbage
+      row :json
+    end
+    active_admin_comments
   end
 end
