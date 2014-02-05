d9ac9ef933bdfb9efaef8d6267d95c58ea43cb50
Andrew Wei <andey.wei@gmail.com>
2014-02-04 19:27:05 -0600

    permit enabled/disable on ApiKeys from admin panel

--- app/admin/api_key.rb
+++ app/admin/api_key.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register ApiKey do
   controller do
     def permitted_params
-      params.permit(:api_key => [:username, :consumer_key, :consumer_secret, :token, :token_secret])
+      params.permit(:api_key => [:username, :consumer_key, :consumer_secret, :token, :token_secret, :enabled])
     end
   end
 
